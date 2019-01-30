ef0a990ac612d9af3edacaa5a21d1a070436e2b8
Jonathan Kuhl <jckuhl87@gmail.com>
2019-01-30 00:07:47 -0500

    click event scaffolding

--- src/components/Mine.js
+++ src/components/Mine.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Mine extends Component {
+
+    state = {
+        uncovered: false
+    }
+
+    sweepMine(event) {
+        event.preventDefault();
+        if (event.type === 'click') {
+            this.setState({ uncovered: true });
+            if(this.props.mine.bomb) {
+                console.log('Boom!');
+            } else if(this.props.mine.squares === 0) {
+                console.log('Clear!');
+            }
+        } else if (event.type === 'contextmenu') {
+            if(!this.state.uncovered) {
+                console.log('Flag')
+            }
+        }
+    }
+
     render() {
         const bomb = '💣'
         return (
-            <div className="mine">
-                { this.props.mine.squares === -1 ? bomb : this.props.mine.squares }  
+            <div className="mine" onClick={this.sweepMine.bind(this)} onContextMenu={this.sweepMine.bind(this)}>
+                { this.props.mine.bomb ? bomb : this.props.mine.squares }  
             </div>
         );
     }
