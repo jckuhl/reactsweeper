3a9f72fe2f93736f4d526413a9ec369ddc7ec704
Jonathan Kuhl <jckuhl87@gmail.com>
2019-03-05 21:29:22 -0500

    cleaned up Copy

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -2,20 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = styled.footer`
-    /* position: fixed;
-    left: 0;
-    bottom: 0px; */
     flex-shrink: 0;
     width: 100%;
     padding: 1rem;
     text-align: center;
     height: 60px;
-`;
-
-const Phantom = styled.div`
-    display: block;
-    height: 60px;
-    width: 100%;
 `;
 
 export default function Copy() {
